6d9afb64f4070ea8896847a1e394153af559c455
vemendez <edumendez6523@gmail.com>
2021-09-16 10:57:50 -0400

    first commit

---
+++ Presentacion/vue-3-reactivity-master/01-basic.js
@@ -0,0 +1,25 @@
+let price = 5
+let quantity = 2
+let total = 0
+
+let dep = new Set()
+
+let effect = () => {
+  total = price * quantity
+}
+
+function track() {
+  dep.add(effect)
+}
+
+function trigger() {
+  dep.forEach(effect => effect())
+}
+
+console.log(`total is ${ total }`)
+track()
+effect()
+console.log(`total is ${ total }`)
+price = 20
+trigger()
+console.log(`total is ${ total }`)
